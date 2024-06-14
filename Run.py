0331f9ac4969a77a18ca447c3ac6ded3298fb412
Nur Mateen <nuurmateen@gmail.com>
2024-06-14 16:43:31 +0330

    Update Run.py

--- Run.py
+++ Run.py
@@ -2,6 +2,6 @@ x1 = float(input("X1 >>> "))
 x2 = float(input("X2 >>> "))
 b = -1*(x1+x2)
 if b<0:
-    print(f"x² {b}x + {x1*x2}")
+    print(f"x² - {b*-1}x + {x1*x2}")
 else:
     print(f"x² + {b}x + {x1*x2}")    
